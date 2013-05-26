7c072a2a2d8a8363dca15b13ef3737297d08cbbc
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-05-25 22:04:24 -0700

    Switch from install to deploy.

--- gcm-client/deploy/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn3 install:install-file -Dmaven.repo.local=../releases -Dfile=gcm.jar -Dsources=gcm-src.jar -DgroupId=com.google.android.gcm -DartifactId=gcm-client -Dversion=1.0.2 -Dpackaging=jar -DcreateChecksum=true
--- gson/deploy/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn3 install:install-file -Dmaven.repo.local=../releases -Dfile=thoughtcrimegson-2.1.jar -DgroupId=org.whispersystems -DartifactId=gson -Dversion=2.1 -Dpackaging=jar -DcreateChecksum=true
